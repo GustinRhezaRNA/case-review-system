8efb26a7ef5a86dea951e802b7f32c6e011dec80
Gustin Rheza <rezarna4@gmail.com>
2026-01-24 15:32:04 +0700

    feat: initial backend commit

---
+++ src/app.module.ts
@@ -0,0 +1,18 @@
+import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { PrismaService } from '../prisma/prisma.service';
+import { AuthMiddleware } from './auth/auth.middleware';
+import { AuthModule } from './auth/auth.module';
+import { CasesModule } from './cases/cases.module';
+
+@Module({
+  imports: [AuthModule, CasesModule],
+  controllers: [AppController],
+  providers: [AppService, PrismaService],
+})
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(AuthMiddleware).forRoutes('*');
+  }
+}
