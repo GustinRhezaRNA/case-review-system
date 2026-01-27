afe554e35c357766167e98868e51e0e3e79a567c
Gustin Rheza <rezarna4@gmail.com>
2026-01-27 22:06:06 +0700

    feat: add protected and public routes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,47 @@
-import { Routes, Route } from 'react-router-dom'
+import { Routes, Route, Navigate } from 'react-router-dom'
 import Dashboard from "./app/(protected)/dashboard/page"
 import CaseDetailPage from "./app/(protected)/dashboard/[id]/page"
+import Login from './app/(public)/login/page'
+import { useAuth } from "./hooks/use-auth"
+import type { JSX } from 'react'
+
+function ProtectedRoute({ children }: { children: JSX.Element }) {
+  const { user } = useAuth()
+
+  if (!user) {
+    return <Navigate to="/login" replace />
+  }
+
+  return children
+}
 
 function App() {
   return (
     <Routes>
-      <Route path="/dashboard" element={<Dashboard />} />
-      <Route path="/dashboard/:id" element={<CaseDetailPage />} />
+      {/* Public */}
+      <Route path="/login" element={<Login />} />
+
+      {/* Protected */}
+      <Route
+        path="/dashboard"
+        element={
+          <ProtectedRoute>
+            <Dashboard />
+          </ProtectedRoute>
+        }
+      />
+
+      <Route
+        path="/dashboard/:id"
+        element={
+          <ProtectedRoute>
+            <CaseDetailPage />
+          </ProtectedRoute>
+        }
+      />
+
+      {/* Default */}
+      <Route path="*" element={<Navigate to="/dashboard" />} />
     </Routes>
   )
 }
