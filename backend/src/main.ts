241f5acaaa0ed4531c91c241d2961942ef60ecd0
Gustin Rheza <rezarna4@gmail.com>
2026-02-13 13:15:32 +0700

    feat: add api versioning

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -5,6 +5,7 @@ import { HttpExceptionFilter } from './common/filters/http-exception.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.setGlobalPrefix('api/v1');
 
   app.useGlobalPipes(
     new ValidationPipe({
