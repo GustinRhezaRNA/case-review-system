cad1c970b7e7fb5b253d1f73fe21d68a6f40f49e
Gustin Rheza <rezarna4@gmail.com>
2026-02-13 13:17:35 +0700

    feat: add git ignore and make the client base url dynamic

--- frontend/src/api/client.ts
+++ frontend/src/api/client.ts
@@ -1,8 +1,7 @@
+import { axiosConfig } from '@/lib/axios/config';
 import axios from 'axios';
 
-export const api = axios.create({
-  baseURL: 'http://localhost:3000',
-});
+export const api = axios.create(axiosConfig);
 
 export const setUserHeader = (userId: string) => {
   api.defaults.headers.common['x-user-id'] = userId;
